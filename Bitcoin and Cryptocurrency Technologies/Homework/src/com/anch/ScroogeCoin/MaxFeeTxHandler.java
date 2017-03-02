46628af950a442d82cb83ed1215e0f6f2201c42e
AnCh7 <anton_chug@yahoo.com>
2017-03-02 13:53:31 +0300

    Added inital solutions for Bitcoin and Cryptocurrency Technologies course

---
+++ Bitcoin and Cryptocurrency Technologies/Homework/src/com/anch/ScroogeCoin/MaxFeeTxHandler.java
@@ -0,0 +1,11 @@
+package com.anch.ScroogeCoin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class MaxFeeTxHandler {
+
+
+}
